fb7403d42937f6d0fe8e310b03177d683d72c0ca
salmansrabon <salmansrabon@gmail.com>
2023-09-22 22:15:20 +0600

    implement POM v1

---
+++ src/test/java/DashboardTestRunner.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class DashboardTestRunner extends Setup {
+    @BeforeTest
+    public void login(){
+        LoginPage loginPage=new LoginPage(driver);
+        loginPage.doLogin("admin","admin123");
+    }
+    @Test(priority = 1)
+    public void createUser(){
+        DashboardPage dashboardPage=new DashboardPage(driver);
+        dashboardPage.createUser("Test","User","testuser100","P@ssword123");
+
+    }
+}
